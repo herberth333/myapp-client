978f707f9765e6df16c7bcf892be219dbcbd05d7
herberth333 <86244914+herberth333@users.noreply.github.com>
2025-07-18 22:38:48 -0300

    feat: SEO inserido para testes

--- brb-client/src/app/page.tsx
+++ brb-client/src/app/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next"; 
+
+
+export const metadata: Metadata = {
+  title: "Home- aula de nextjs",
+  description: "aprendendo nextjs",
+  openGraph: {
+    title: "Aprendendo com o cara do yt",
+    description:"teste da descrição pelo whatsapp",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    }
+  }
+};
+
 export default function Home(){
   return(
     <div>
